953d7680be69b6e12da355f7d49881ec79335307
minjun7410 <minjun7410@gmail.com>
2023-11-09 22:22:10 +0900

    test(DayTest): 주문 날짜에 별이 있는 지 확인하는 기능 테스트 추가
    * 해당 날짜가 별 체크된 날짜인지 확인하는 기능 테스트 추가

--- src/test/java/christmas/DayTest.java
+++ src/test/java/christmas/DayTest.java
@@ -2,6 +2,8 @@ package christmas;
 
 import christmas.domain.Day;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -16,5 +18,12 @@ public class DayTest {
     void 주문_날짜_범위_밖_테스트() {
         Day day = new Day(30);
         assertThat(day.isInBetween(1, 25)).isFalse();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {3, 25, 5, 29}, booleans = {true, true, false, false})
+    void 주문_날짜_별_테스트(int input, boolean result) {
+        Day day = new Day(input);
+        assertThat(day.isStarDay()).isEqualTo(result);
     }
 }
