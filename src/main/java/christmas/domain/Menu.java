fe14efc1b87358732a7b8a1f0b0d83c2d9a9d38f
minjun7410 <minjun7410@gmail.com>
2023-11-09 23:21:38 +0900

    feat(Menu, MenuType): 주문한 메뉴가 디저트, 메인 메뉴인지 확인하는 기능 구현
    * 메뉴 상수를 다루는 Enum 클래스 Menu 추가
    * 메뉴의 타입 상수를 다루는 Enum 클래스 MenuType 추가
    * Menu: 해당 메뉴가 인자로 들어온 MenuType 인지 확인하는 메서드 추가

---
+++ src/main/java/christmas/domain/Menu.java
@@ -0,0 +1,30 @@
+package christmas.domain;
+
+public enum Menu {
+    MUSHROOMSOUP("양송이수프", 6000, MenuType.APPETIZER),
+    TAPAS("타파스", 5500, MenuType.APPETIZER),
+    CAESAR_SALAD("시저샐러드", 5500, MenuType.APPETIZER),
+    T_BONE_STEAK("티본스테이크", 55000, MenuType.MAIN),
+    BBQ_LIP("바베큐립", 54000, MenuType.MAIN),
+    SEAFOOD_PASTA("해산물파스타", 35000, MenuType.MAIN),
+    CHRISMAS_PASTA("크리스마스파스타", 25000, MenuType.MAIN),
+    CHOCO_CAKE("초코케이크", 15000, MenuType.DESERT),
+    ICECREAM("아이스크림", 5000, MenuType.DESERT),
+    ZERO_COLA("제로콜라", 3000, MenuType.DRINK),
+    RED_WINE("레드와인", 60000, MenuType.DRINK),
+    CHAMPAGNE("샴페인", 25000, MenuType.DRINK);
+
+    private final String name;
+    private final int price;
+    private final MenuType menuType;
+
+    Menu(String name, int price, MenuType menuType) {
+        this.name = name;
+        this.price = price;
+        this.menuType = menuType;
+    }
+
+    public boolean compareType(MenuType menuType) {
+        return (this.menuType == menuType);
+    }
+}
---
+++ src/main/java/christmas/domain/MenuType.java
@@ -0,0 +1,5 @@
+package christmas.domain;
+
+public enum MenuType {
+    MAIN, DESERT, APPETIZER, DRINK
+}
