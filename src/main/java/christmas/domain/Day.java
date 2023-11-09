be88998ed4bb0d3d7a483cfd32be1e41a20c7a40
minjun7410 <minjun7410@gmail.com>
2023-11-09 22:44:12 +0900

    feat(Day): 날짜가 1~31 범위에 있는지 예외 처리하는 기능 구현
    * 1~31 범위에 있는 날짜가 아닌 경우 예외 처리하는 기능 구현

--- src/main/java/christmas/domain/Day.java
+++ src/main/java/christmas/domain/Day.java
@@ -3,11 +3,16 @@ package christmas.domain;
 import java.util.List;
 
 public class Day {
+    private static final int START_DAY_OF_MONTH = 1;
+    private static final int END_DAY_OF_MONTH = 31;
+    private static final String INVALID_DAY_TEXT = "유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+
     private final List<Integer> starDays = List.of(3, 10, 17, 24, 25, 31);
     int day;
 
     public Day(int day) {
         this.day = day;
+        validateDayRange();
     }
 
     public boolean isInBetween(int start, int end) {
@@ -16,5 +21,10 @@ public class Day {
 
     public boolean isStarDay() {
         return starDays.contains(day);
+    }
+
+    private void validateDayRange() {
+        if (START_DAY_OF_MONTH <= day && day <= END_DAY_OF_MONTH) return;
+        throw new IllegalArgumentException(INVALID_DAY_TEXT);
     }
 }
