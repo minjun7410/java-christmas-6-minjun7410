20424743534a33be3f73e26d8de652361c05aac3
minjun7410 <minjun7410@gmail.com>
2023-11-10 19:33:34 +0900

    feat(OrderSheetTest): 총 주문 갯수가 20개를 넘는지 예외처리하는 기능 구현
    * 총 주문 갯수가 20개를 넘었을 때 IllegalArgumentException 예외처리하는 기능 구현

--- src/main/java/christmas/domain/OrderSheet.java
+++ src/main/java/christmas/domain/OrderSheet.java
@@ -5,9 +5,11 @@ import java.util.List;
 import java.util.Map;
 
 public class OrderSheet {
+    private static final String EXCEED_MENU_COUNT_TEXT = "유효하지 않은 주문입니다. 다시 입력해 주세요.";
     private Map<Menu, Integer> orderSheet;
 
     public OrderSheet(Map<String, Integer> menus) {
+        validateExceedMenuCount(menus);
         orderSheet = new HashMap<>();
         countMenu(menus);
     }
@@ -32,5 +34,12 @@ public class OrderSheet {
             Menu menu = Menu.getMenuByName(menuName);
             orderSheet.put(menu, menus.get(menuName));
         }
+    }
+
+    private void validateExceedMenuCount(Map<String, Integer> menus) {
+        int totalMenuCount = menus.values().stream()
+                .reduce(0, Integer::sum);
+        if (totalMenuCount <= 20) return;
+        throw new IllegalArgumentException(EXCEED_MENU_COUNT_TEXT);
     }
 }
