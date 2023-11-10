83cbb2fafde24c1c1cf060f1d197a8955b0a8e72
minjun7410 <minjun7410@gmail.com>
2023-11-10 14:43:52 +0900

    feat(StarEvent): 특별 할인 금액 1000원 적용 기능 구현
    * 할인 금액 1000원을 리턴하는 getDiscountedAmount 메서드 추가

--- src/main/java/christmas/domain/StarEvent.java
+++ src/main/java/christmas/domain/StarEvent.java
@@ -1,6 +1,7 @@
 package christmas.domain;
 
 public class StarEvent {
+    private static final int DISCOUNT_AMOUNT = 1000;
     private final Day presentDay;
 
     public StarEvent(Day presentDay) {
@@ -9,5 +10,9 @@ public class StarEvent {
 
     public boolean isDiscountable() {
         return presentDay.isStarDay();
+    }
+
+    public int getDiscountedAmount() {
+        return DISCOUNT_AMOUNT;
     }
 }
