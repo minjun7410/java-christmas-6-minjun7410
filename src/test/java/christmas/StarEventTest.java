45f2c9108cbd062bc7f209c77626155efda76ffc
minjun7410 <minjun7410@gmail.com>
2023-11-10 14:29:43 +0900

    test(StarEventTest): 별 이벤트 할인이 적용 가능한지 확인하는 기능 테스트 추가
    * 별 표시 되어있는 날이 적용되는 지 확인하는 테스트 추가
    * 되어 있지 않은 날이 적용되지 않았는지 확인하는 테스트 추가

---
+++ src/test/java/christmas/StarEventTest.java
@@ -0,0 +1,23 @@
+package christmas;
+
+import christmas.domain.Day;
+import christmas.domain.StarEvent;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class StarEventTest {
+    @Test
+    void 할인_적용_가능_테스트() {
+        Day day = new Day(25);
+        StarEvent starEvent = new StarEvent(day);
+        assertThat(starEvent.isDiscountable()).isTrue();
+    }
+
+    @Test
+    void 할인_적용_가능_테스트() {
+        Day day = new Day(26);
+        StarEvent starEvent = new StarEvent(day);
+        assertThat(starEvent.isDiscountable()).isFalse();
+    }
+}
