f0266f60e5be3fd6253d2e5f55662bd026a1778a
minjun7410 <minjun7410@gmail.com>
2023-11-10 11:25:34 +0900

    refactor(WeekDay,WeekDayTest): WeekDay 클래스 이름 변경
    * 평일과 이름이 겹쳐 요일을 의미하도록 WeekDay -> DayOfWeek 으로 이름 변경

---
+++ src/main/java/christmas/domain/DayOfWeek.java
@@ -0,0 +1,30 @@
+package christmas.domain;
+
+public enum DayOfWeek {
+
+    FRIDAY(1),
+    SATURDAY(2),
+    SUNDAY(3),
+    MONDAY(4),
+    TUESDAY(5),
+    WEDNESDAY(6),
+    THURSDAY(7),
+    NOT_DAY(8);
+
+    private int firstDay;
+    private DayOfWeek(int firstDay) {
+        this.firstDay = firstDay;
+    }
+
+    public static boolean isHoliday(int day) {
+        DayOfWeek dayOfWeek = getDayOfWeekByDay(day);
+        return (dayOfWeek == FRIDAY || dayOfWeek == SATURDAY);
+    }
+
+    private static DayOfWeek getDayOfWeekByDay(int day) {
+        for (DayOfWeek dayOfWeek : DayOfWeek.values()) {
+            if ((dayOfWeek.firstDay % 7) == (day % 7)) return dayOfWeek;
+        }
+        return NOT_DAY;
+    }
+}
--- src/test/java/christmas/DayOfWeekTest.java
+++ src/test/java/christmas/DayOfWeekTest.java
@@ -1,21 +1,21 @@
 package christmas;
 
-import christmas.domain.WeekDay;
+import christmas.domain.DayOfWeek;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-public class WeekDayTest {
+public class DayOfWeekTest {
     @ParameterizedTest
     @ValueSource(ints = {1, 9})
     void 주말_요일_테스트(int input) {
-        assertThat(WeekDay.isHoliday(input)).isTrue();
+        assertThat(DayOfWeek.isHoliday(input)).isTrue();
     }
 
     @ParameterizedTest
     @ValueSource(ints = {3, 13})
     void 평일_요일_테스트(int input) {
-        assertThat(WeekDay.isHoliday(input)).isFalse();
+        assertThat(DayOfWeek.isHoliday(input)).isFalse();
     }
 }
