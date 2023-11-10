093876bc63aa2f41070b2d33013ecb5c5476bf71
minjun7410 <minjun7410@gmail.com>
2023-11-10 17:58:56 +0900

    test(MenuTest): 메뉴에 있는지 예외처리하는 기능 테스트
    * 메뉴판에 없는 메뉴 주문하면 예외처리하는 기능 테스트 코드 추가

--- src/test/java/christmas/MenuTest.java
+++ src/test/java/christmas/MenuTest.java
@@ -5,6 +5,7 @@ import christmas.domain.MenuType;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class MenuTest {
     @Test
@@ -17,5 +18,10 @@ public class MenuTest {
     void 주문한_메뉴_메인메뉴_테스트() {
         Menu menu = Menu.BBQ_LIP;
         assertThat(menu.compareType(MenuType.MAIN)).isTrue();
+    }
+
+    @Test
+    void 메뉴에_있는지_예외처리_테스트() {
+        assertThatThrownBy(() -> Menu.getMenuByName("케잌")).isInstanceOf(IllegalArgumentException.class);
     }
 }
