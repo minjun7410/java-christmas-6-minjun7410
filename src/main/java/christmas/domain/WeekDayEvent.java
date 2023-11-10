a42cbc4c7429b7adfe541736887752344ebfbb54
minjun7410 <minjun7410@gmail.com>
2023-11-10 12:55:18 +0900

    feat(WeekDayEvent): 디저트 메뉴 갯수로 할인 금액을 계산하는 기능 구현
    * 메뉴의 타입을 확인하고 카운트하여 할인하는 메서드 getDiscountedAmount 구현

--- src/main/java/christmas/domain/WeekDayEvent.java
+++ src/main/java/christmas/domain/WeekDayEvent.java
@@ -1,8 +1,12 @@
 package christmas.domain;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class WeekDayEvent {
+    private static final int UNIT_AMOUNT = 2023;
+
     private final Day presentDay;
     private final Map<Menu, Integer> orderSheet;
 
@@ -13,5 +17,13 @@ public class WeekDayEvent {
 
     public boolean isDiscountable() {
         return !(presentDay.isHoliday());
+    }
+
+    public int getDiscountedAmount() {
+        int discountedAmount = orderSheet.keySet().stream()
+                .filter(menu -> menu.compareType(MenuType.DESERT))
+                .map(orderSheet::get)
+                .reduce(0, Integer::sum);
+        return UNIT_AMOUNT * discountedAmount;
     }
 }
