c292804857189d67a70ba771c9c0f272444291cb
minjun7410 <minjun7410@gmail.com>
2023-11-10 19:28:25 +0900

    test(OrderSheetTest): 총 주문 갯수가 20개를 넘는지 예외처리하는 기능 테스트 추가
    * 총 주문 갯수가 20개를 넘었을 때 IllegalArgumentException 예외처리하는 기능 테스트 추가

--- src/test/java/christmas/OrderSheetTest.java
+++ src/test/java/christmas/OrderSheetTest.java
@@ -3,6 +3,8 @@ package christmas;
 import christmas.domain.Menu;
 import christmas.domain.OrderSheet;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.internal.matchers.Or;
 
 import java.util.HashMap;
@@ -10,6 +12,7 @@ import java.util.HashSet;
 import java.util.Map;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class OrderSheetTest {
     @Test
@@ -21,5 +24,12 @@ public class OrderSheetTest {
 
         int answer = Menu.BBQ_LIP.getPrice() * 2 + Menu.ICECREAM.getPrice();
         assertThat(orderSheet.getTotalPrice()).isEqualTo(answer);
+    }
+
+    @Test
+    void 총_주문_갯수_초과_예외처리_테스트() {
+        Map<String, Integer> menuCounts = new HashMap<>();
+        menuCounts.put("바비큐립", 21);
+        assertThatThrownBy(() -> new OrderSheet(menuCounts)).isInstanceOf(IllegalArgumentException.class);
     }
 }
