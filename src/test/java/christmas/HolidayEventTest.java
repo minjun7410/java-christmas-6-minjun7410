71b0ba6660fb19f069ba024a13ad3f90af066821
minjun7410 <minjun7410@gmail.com>
2023-11-10 13:15:24 +0900

    test(HolidayEventTest): 메인 메뉴 갯수로 할인 금액을 계산하는 기능 테스트 추가
    * 2가지 메인 메뉴를 시키고 평일에 주문했을 떄 주문금액을 테스트하는 코드 추가

--- src/test/java/christmas/HolidayEventTest.java
+++ src/test/java/christmas/HolidayEventTest.java
@@ -26,4 +26,14 @@ public class HolidayEventTest {
         HolidayEvent holidayEvent = new HolidayEvent(day, orderSheet);
         assertThat(holidayEvent.isDiscountable()).isFalse();
     }
+
+    @Test
+    void 할인_금액_계산_기능_테스트() {
+        Day day = new Day(1);
+        Map<Menu, Integer> orderSheet = new HashMap<>();
+        orderSheet.put(Menu.BBQ_LIP, 1);
+        orderSheet.put(Menu.T_BONE_STEAK, 1);
+        HolidayEvent holidayEvent = new HolidayEvent(day, orderSheet);
+        assertThat(holidayEvent.getDiscountedAmount()).isEqualTo(2023 * 2);
+    }
 }
