06bef8079aea1f061c35cae44dc691d4571283df
minjun7410 <minjun7410@gmail.com>
2023-11-10 18:01:58 +0900

    feat(Menu): 메뉴에 있는지 예외처리하는 기능 구현
    * 메뉴판에 없는 메뉴 주문하면 예외처리하는 기능 구현

--- src/main/java/christmas/domain/Menu.java
+++ src/main/java/christmas/domain/Menu.java
@@ -27,4 +27,11 @@ public enum Menu {
     public boolean compareType(MenuType menuType) {
         return (this.menuType == menuType);
     }
+
+    public static Menu getMenuByName(String menuName) {
+        for (Menu menu : Menu.values()) {
+            if (menu.name.equals(menuName)) return menu;
+        }
+        throw new IllegalArgumentException("유효하지 않은 주문입니다. 다시 입력해 주세요.");
+    }
 }
