4ce63278c636ce4df7042494d9dc7db1d9dda595
minjun7410 <minjun7410@gmail.com>
2023-11-11 18:41:48 +0900

    test(PresentationEventManagerTest): 증정 적용 가능한 이벤트들을 구하는 기능 테스트 추가
    * 12만원 이상 시켰을 때 샴페인을 증정하는 지 / 증정하지 않는지 테스트하는 코드 추가
    * ChampagnePresentationDiscountEventTest: 이름에 샴페인 추가

---
+++ src/test/java/christmas/event/presentation/PresentationEventManagerTest.java
@@ -0,0 +1,34 @@
+package christmas.event.presentation;
+
+import christmas.domain.Menu;
+import christmas.domain.OrderSheet;
+import christmas.domain.event.presentation.PresentationEventManager;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class PresentationEventManagerTest {
+    @Test
+    void 샴페인_증정_이벤트_테스트() {
+        Map<String, Integer> menuCounts = new HashMap<>();
+        menuCounts.put("바비큐립", 5);
+        menuCounts.put("아이스크림", 1);
+        OrderSheet orderSheet = new OrderSheet(menuCounts);
+        List<Menu> presentations = PresentationEventManager.getPresentations(orderSheet);
+        assertThat(presentations.get(0)).isEqualTo(Menu.CHAMPAGNE);
+    }
+
+    @Test
+    void 샴페인_증정_이벤트_미증정_테스트() {
+        Map<String, Integer> menuCounts = new HashMap<>();
+        menuCounts.put("바비큐립", 1);
+        menuCounts.put("아이스크림", 1);
+        OrderSheet orderSheet = new OrderSheet(menuCounts);
+        List<Menu> presentations = PresentationEventManager.getPresentations(orderSheet);
+        assertThat(presentations.size()).isEqualTo(0);
+    }
+}
