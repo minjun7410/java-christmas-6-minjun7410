7b4505267a3b91051eeaffe332270c4c048a2566
minjun7410 <minjun7410@gmail.com>
2023-11-11 17:53:35 +0900

    feat(DiscountEventManager): 할인 적용 가능한 이벤트들을 구하는 기능 구현
    * 할인 적용 가능 이벤트를 구하는 기능 구현

---
+++ src/main/java/christmas/domain/event/DiscountEventManager.java
@@ -0,0 +1,29 @@
+package christmas.domain.event;
+
+import christmas.domain.Day;
+import christmas.domain.OrderSheet;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BiFunction;
+
+public enum DiscountEventManager {
+    CHRISTMAS((day, orderSheet) -> new ChristmasDiscountEvent(day)),
+    HOLIDAY((day, orderSheet) -> new HolidayDiscountEvent(day, orderSheet)),
+    WEEKDAY((day, orderSheet) -> new WeekDayDiscountEvent(day, orderSheet)),
+    STAR(((day, orderSheet) -> new StarDiscountEvent(day)));
+
+    private BiFunction<Day, OrderSheet, DiscountEvent> createFunction;
+
+    private DiscountEventManager(BiFunction<Day, OrderSheet, DiscountEvent> createFunction) {
+        this.createFunction = createFunction;
+    }
+
+    public static List<DiscountEvent> getDiscountableEvents(Day day, OrderSheet orderSheet) {
+        List<DiscountEvent> discountEvents = Arrays.stream(values())
+                .map(event -> event.createFunction.apply(day, orderSheet))
+                .filter(DiscountEvent::isDiscountable)
+                .toList();
+        return discountEvents;
+    }
+}
