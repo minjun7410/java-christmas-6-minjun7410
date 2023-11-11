ec72b3c35bf41b6c7fe56ea73379559c589d8ce1
minjun7410 <minjun7410@gmail.com>
2023-11-11 19:34:40 +0900

    feat(InputView): 중복된 메뉴가 있는지 예외처리하는 기능 구현
    * 이미 입력한 메뉴가 다시 입력되면 예외처리하는 기능 구현

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -2,8 +2,16 @@ package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class InputView {
     private static final String WRONG_DAY_TEXT = "유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+    private static final String WRONG_TYPE_TEXT = "유효하지 않은 주문입니다. 다시 입력해 주세요.";
+    private static final String NO_MENU_TEXT = "유효하지 않은 주문입니다. 다시 입력해 주세요.";
+    private static final String DUPLICATE_MENU_TEXT = "유효하지 않은 주문입니다. 다시 입력해 주세요.";
+
     public int readDate() {
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
         String input = Console.readLine();
@@ -23,5 +31,9 @@ public class InputView {
 
     private void validateNoMenu(String[] inputs) {
         if (inputs.length == 0) throw new IllegalArgumentException(NO_MENU_TEXT);
+    }
+
+    private void validateDuplicatedMenu(Set<String> menuNames, String menuName) {
+        if (menuNames.contains(menuName)) throw new IllegalArgumentException(DUPLICATE_MENU_TEXT);
     }
 }
