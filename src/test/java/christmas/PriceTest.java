ae83cbdbbdcf331e45db6d416053573c34894728
minjun7410 <minjun7410@gmail.com>
2023-11-12 22:16:50 +0900

    test(PriceTest): 예상 결제 금액이 0보다 작을 경우 0으로 리턴하는 기능 테스트 추가
    * 0보다 작은 금액이 나올 경우 0으로 리턴하는 기능 테스트 추가

--- src/test/java/christmas/PriceTest.java
+++ src/test/java/christmas/PriceTest.java
@@ -13,6 +13,13 @@ public class PriceTest {
         assertThat(getMatchCount(priceText, ",")).isEqualTo(2);
     }
 
+    @Test
+    void 금액이_0보다_작을때_0으로_계산하는_기능_테스트() {
+        Price price = new Price(1000);
+        price.discount(2000);
+        assertThat(price.toString()).isEqualTo("0");
+    }
+
     private int getMatchCount(String input, String target) {
         String replacedInput = input.replace(target, "");
         return input.length() - replacedInput.length();
